8f462ad27d47ac289f6f04aaaa55604b09e473c2
jrajan20 <jai_20j@hotmail.com>
2026-06-30 10:11:01 -0400

    getting lat and lng from url

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,16 @@
 
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
+   const [searchParams, setSearchParams] = useSearchParams();
+   const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+
     return (
         <div className={styles.mapContainer}>
-            Map
+           <h1>Map</h1>
+              <p>Latitude: {lat}</p>
+              <p>Longitude: {lng}</p>
         </div>
     )
 }
